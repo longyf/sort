8dd5257c263108f12c8dfc4817d132af88f3ba7a
longyf <longyf@pku.edu.cn>
2018-04-07 11:07:26 +0800

    Modify insertSort
    simplification:
        do not need to use extra array

--- insertSort.h
+++ insertSort.h
@@ -10,17 +10,10 @@ void insertSort(T num[], int length) {
 	if (num==nullptr||length<=0)
 		throw invalid_argument("Invalid array in insertSort.");
 
-	T *temp=new T [length];
-	temp[0]=num[0];
-
 	for (int size=1; size!=length; ++size) {
-		insert(temp, size, num[size]);
+		T temp=num[size];
+		insert(num, size, temp);
 	}
 
-	for (int i=0; i!=length; ++i) {
-		num[i]=temp[i];
-	}
-
-	delete []temp;
 }
 #endif
