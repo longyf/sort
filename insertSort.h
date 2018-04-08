b0a814b4da2d68616b9f8dd6e2dedade93317dbc
longyf <longyf@pku.edu.cn>
2018-04-08 09:03:33 +0800

    Add insertSort_2
    put insert and insertSort in one function.

--- insertSort.h
+++ insertSort.h
@@ -16,4 +16,21 @@ void insertSort(T num[], int length) {
 	}
 
 }
+
+template <class T>
+void insertSort_2(T num[], int length) {
+	if (num==nullptr||length<=0)
+		throw invalid_argument("Invalid array in insertSort_2.");
+
+	for (int size=1; size!=length; ++size) {
+		int temp=num[size];
+		int i=size-1;
+		for (; i!=-1&&temp<num[i]; --i) {
+			num[i+1]=num[i];
+		}
+		num[i+1]=temp;
+	}
+
+}
+		
 #endif
--- main.cpp
+++ main.cpp
@@ -8,14 +8,15 @@
 using namespace std;
 
 int main() {
-	const int length=10;
-	int a[length]={0,1,2,4,3,5,6,7,8,9};
+	const int length=2;
+//	int a[length]={0,1,2,4,3,5,6,7,8,9};
+//	int a[length]={9,8,7,6,5,4,3,2,1,0};
 //	int a[length]={1,0,2,3,4,5,6,7,8,9};
-//	int a[length]={1};
+	int a[length]={1,1};
 //	selectionSort_2(a,length);//example.
 //	selectionSortv2(a,length);//my version.
 //	bubbleSort(a,length);
-	insertSort(a,length);
+	insertSort_2(a,length);
 	output(a,length);
 	
 	return 0;
